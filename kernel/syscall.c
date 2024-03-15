597888e28cc2db950029a0d256e5d64b433588c8
sjr <1779229023@qq.com>
2024-03-15 10:14:50 +0800

    my work on challengex is done.

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -17,6 +17,66 @@
 #include "elf.h"
 #include "spike_interface/spike_utils.h"
 
+
+extern symbol sh[32];
+extern elf_section ini;
+extern uint64 cot;
+extern uint64 id[64];
+
+ssize_t sys_user_lab1_challenge1(uint64 a1)
+{
+  uint64 n=1;
+  uint64 i=0; 
+  assert(current);
+  //uint64 addr1=current->trapframe->kernel_sp;
+  //USER_STACK
+  //sp是高地址
+  //sprint("kstack:%lx\n",current->trapframe->regs.s0);
+  //
+  //print的栈最高地址0x810fff70 0x810ffff70-8返回地址为0x8100000e即调用print的下一个地址 0x810ffff70-16为0x810fff80下一个函数栈为f7引用f8存f8的返回地址
+  //栈标识的fp分别为0x810fff70 0x810fff80 0x810fff90 0x810fffa0...
+  //
+ uint64 ar1=current->trapframe->regs.s0+16;//s0寄存器保存函数调用的栈顶
+//  for(i=0;i<=11;i++)
+//   sprint("%lx\n",sh[i].offset);
+//  sprint("%lx\n",*(uint64 *)(ar1-8));
+   while(n<=a1)
+    {
+    
+   for(i=1;i<=sizeof(sh);i++)
+   {
+    
+      if(*(uint64 *)(ar1-8)>=sh[i].offset&&*(uint64 *)(ar1-8)<sh[i+1].offset)
+      {
+        //sprint("%ld",i);
+        if(strcmp(sh[i].name,"main")==0)
+        {
+          n=a1;
+        }
+        //sprint("%lx\n",*(uint64 *)ar1-8);
+        sprint("%s\n",sh[i].name);
+        break;
+      }
+     }
+      ar1= *(uint64 *)(ar1-16);
+      n+=1;
+  }
+//  for(i=0;i<=10;i++)
+//   {
+//   sprint("%ld号地址内容:%lx\n",i,*(uint64 *)(0x810fff70-i*8));
+//      //读栈
+//  }
+// 读elf
+  // for(i=0;i<=10;i++)
+  // {
+  //   sprint("elf:%s\n",sh[i].name);
+  // }
+  // sprint("elf_sect:%lx\n",cot);
+  //sprint("kstack:%lx\n",current->trapframe->epc);
+  return 0;
+}
+
+
 void syscall_load_bincode_from_host_elf(process *p,char *s,char *para) {
   //arg_buf arg_bug_msg;
 
@@ -372,7 +432,7 @@ long do_syscall(long a0, long a1, long a2, long a3, long a4, long a5, long a6, l
     case SYS_user_exec:
       return sys_user_exec((char *)a1,(char *)a2);
     case SYS_user_lab1_challenge1:
-      return  ;
+      return sys_user_lab1_challenge1(a1);
     default:
       panic("Unknown syscall %ld \n", a0);
   }
