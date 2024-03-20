b60a7f9e6fb161565bff7bf7517a5c5cc1b465e7
sjr <1779229023@qq.com>
2024-03-20 16:00:04 +0800

    my work on challengex is done.

---
+++ user/app_exec_challengex.c
@@ -0,0 +1,13 @@
+#include "user_lib.h"
+#include "util/types.h"
+
+int main(int argc, char *argv[]) {
+  printu("\n======== exec /bin/app_ls in app_exec ========\n");
+  printu("call exec in main\n");
+  int ret = exec("/bin/app_ls");
+  if (ret == -1)
+    printu("exec failed!\n");
+
+  exit(0);
+  return 0;
+}
