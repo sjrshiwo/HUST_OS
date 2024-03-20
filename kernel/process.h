9ddefa4bdc38917dd3010b07f894c7fecd0dcf68
sjr <1779229023@qq.com>
2024-03-20 10:01:52 +0800

    my work on challengex is done.

--- kernel/process.h
+++ kernel/process.h
@@ -100,8 +100,8 @@ typedef struct process_t {
 
   // file system. added @lab4_1
   proc_file_management *pfiles;
-   char *debugline; char **dir; code_file *file; addr_line *line; 
-   int line_ind;
+  char *debugline; char **dir; code_file *file; addr_line *line; 
+  int line_ind;
 }process;
 
 // switch to run user app
